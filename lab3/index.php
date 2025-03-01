d4ed92c5362fa3bbcddd18a582f3e7b333d98661
lkarapetyashka <lianna.karapetyan.95@mail.ru>
2025-03-01 16:57:07 +0300

    Update index.php

--- lab3/index.php
+++ lab3/index.php
@@ -72,9 +72,9 @@ if ($errors) {
 
 // Сохранение в базу данных.
 
-$user = 'u68791'; //
-$pass = '1609462'; 
-$db = new PDO('mysql:host=localhost;dbname=u68791', $user, $pass,
+$user = 'u68792'; //
+$pass = '1210453'; 
+$db = new PDO('mysql:host=localhost;dbname=u68792', $user, $pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
 
 $inQuery = implode(',', array_fill(0, count($language), '?'));
