302b06268fbde20c5414608013aa8a12f65c4f77
lkarapetyashka <lianna.karapetyan.95@mail.ru>
2025-04-16 20:27:50 +0300

    Delete lab4/sign_in.php

--- lab4/sign_in.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$user = 'u68792'; 
-	$pass = '1210453'; 
-	$db = new PDO('mysql:host=localhost;dbname=u68792', $user, $pass,
-	[PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
-?>
