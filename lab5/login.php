2268dcdf1abddf1ee3711a8b59c5f0720fdb30f3
lkarapetyashka <lianna.karapetyan.95@mail.ru>
2025-04-16 20:39:18 +0300

    Update login.php

--- lab5/login.php
+++ lab5/login.php
@@ -12,7 +12,7 @@
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST')
     {
-        require('database.php');
+        require('sign_in.php');
         $login = $_POST['login'];
         $password = md5($_POST['password']);
         try
