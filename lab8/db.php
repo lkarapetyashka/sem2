52846e655e3daaf467fb6d6e170c182fa196b710
lkarapetyashka <lianna.karapetyan.95@mail.ru>
2025-05-27 11:28:34 +0300

    Update db.php

--- lab8/db.php
+++ lab8/db.php
@@ -1,9 +1,9 @@
 <?php
 //файл соединения с базой данных mysql
 $host   = 'localhost';
-$dbname = 'u68687';
-$user   = 'u68687';
-$pass   = '8401584';
+$dbname = 'u68792';
+$user   = 'u68792';
+$pass   = '1210453';
 //Подключаемся к базе
 $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
